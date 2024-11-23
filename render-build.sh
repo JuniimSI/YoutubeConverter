98d057eddcb6c631b35599bd398f8cdad7fe868c
Juniimf <jrfernandesti@gmail.com>
2024-11-23 16:19:59 -0300

    feat: added chrome instable

--- render-build.sh
+++ render-build.sh
@@ -2,3 +2,8 @@
 # Baixar o yt-dlp para o diretório do projeto
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
 chmod +x yt-dlp
+# Instalar Chrome
+apt-get update && apt-get install -y wget gnupg
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update && apt-get install -y google-chrome-stable
