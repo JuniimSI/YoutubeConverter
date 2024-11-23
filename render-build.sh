e1946f58ad18e71a29f435845766d7ae270d5f85
Juniimf <jrfernandesti@gmail.com>
2024-11-23 17:03:07 -0300

    refactor: setting local ytb

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,12 @@
-#!/usr/bin/env bash
-# Baixar o yt-dlp para o diretório do projeto
+# Instala dependências necessárias
+echo "Instalando yt-dlp..."
+
+# Baixar e instalar o yt-dlp
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
 chmod +x yt-dlp
+
+# Outras dependências do seu projeto (se necessário)
+echo "Instalando dependências do npm..."
+npm install
+
+echo "yt-dlp instalado e dependências do npm concluídas!"
